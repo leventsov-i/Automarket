2af91714184fdf8b75b73766d3a5ce6ffd6dd3de
ivan.leventsov <L11cov@yandex.ru>
2020-10-10 20:57:58 +0300

    add dealer to recognize service

--- src/main/java/ru/vtb/recognize/service/RecognizeCarService.java
+++ src/main/java/ru/vtb/recognize/service/RecognizeCarService.java
@@ -3,14 +3,17 @@ package ru.vtb.recognize.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import ru.vtb.marketplace.CarDealerService;
 import ru.vtb.marketplace.MarketplaceService;
 import ru.vtb.marketplace.pojo.CarInfo;
 import ru.vtb.marketplace.pojo.CarInfoResponse;
+import ru.vtb.marketplace.pojo.Dealer;
 import ru.vtb.recognize.dto.RecognizeCarRequestVtbApiDto;
 import ru.vtb.recognize.dto.RecognizeCarResponseVtbApiDto;
 import ru.vtb.recognize.dto.RecognizeRequestDto;
 import ru.vtb.recognize.dto.RecognizeResponseDto;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
@@ -19,11 +22,13 @@ import java.util.Optional;
 public class RecognizeCarService {
     private final RecognizeCarRequestVtbApiExecutor requestVtbApiExecutor;
     private final MarketplaceService marketplaceService;
+    private final CarDealerService carDealerService;
 
     @Autowired
-    public RecognizeCarService(RecognizeCarRequestVtbApiExecutor requestVtbApiExecutor, MarketplaceService marketplaceService) {
+    public RecognizeCarService(RecognizeCarRequestVtbApiExecutor requestVtbApiExecutor, MarketplaceService marketplaceService, CarDealerService carDealerService) {
         this.requestVtbApiExecutor = requestVtbApiExecutor;
         this.marketplaceService = marketplaceService;
+        this.carDealerService = carDealerService;
     }
 
     public RecognizeResponseDto recognizeCar(RecognizeRequestDto request) {
@@ -37,10 +42,11 @@ public class RecognizeCarService {
 
         String carName = max.isEmpty() ? null : max.get().getKey();
         Optional<CarInfo> carInfo = marketplaceService.getCarInfo(carName);
+        List<Dealer> dealers = carInfo.map(info -> carDealerService.getDealers(info.getBrand())).orElse(List.of());
         RecognizeResponseDto recognizeResponseDto = new RecognizeResponseDto(
                 max.isPresent(),
                 carName,
-                new CarInfoResponse(carInfo.isPresent(), carInfo)
+                new CarInfoResponse(carInfo.isPresent(), carInfo, dealers)
         );
         log.info("Answer: {}. Probability: {}", recognizeResponseDto, responseFromVtbApi);
         return recognizeResponseDto;
