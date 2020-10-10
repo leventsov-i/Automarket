d64db12a30bc76ae691948792c0dbc6f0f659309
ivan.leventsov <L11cov@yandex.ru>
2020-10-10 14:22:23 +0300

    not send null field

--- src/main/java/ru/vtb/recognize/dto/RecognizeResponseDto.java
+++ src/main/java/ru/vtb/recognize/dto/RecognizeResponseDto.java
@@ -1,10 +1,12 @@
 package ru.vtb.recognize.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RecognizeResponseDto {
     private boolean found;
     private String carName;
