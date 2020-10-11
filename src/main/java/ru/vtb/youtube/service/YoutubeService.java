7885800375c21c12b6cf7c19ee01bb062b6f9929
Denis Panin <denis@denispanin.com>
2020-10-11 12:23:07 +0300

    Add cache to youtube requests

--- src/main/java/ru/vtb/youtube/service/YoutubeService.java
+++ src/main/java/ru/vtb/youtube/service/YoutubeService.java
@@ -1,5 +1,7 @@
 package ru.vtb.youtube.service;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import com.github.benmanes.caffeine.cache.LoadingCache;
 import com.google.common.collect.Lists;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -7,11 +9,17 @@ import ru.vtb.youtube.dto.exclude.BaseYoutubeResponseDto;
 import ru.vtb.youtube.dto.include.YoutubeSearchVideoResponseDto;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
 public class YoutubeService {
     private final YoutubeSearchVideoRequestExecutor youtubeSearchVideoRequestExecutor;
+    private final LoadingCache<String, List<YoutubeSearchVideoResponseDto>> cache = Caffeine.newBuilder()
+            .maximumSize(1000)
+            .expireAfterWrite(1, TimeUnit.HOURS)
+            .build(this::searchVideoInternal);
+
 
     @Autowired
     public YoutubeService(YoutubeSearchVideoRequestExecutor youtubeSearchVideoRequestExecutor) {
@@ -19,6 +27,10 @@ public class YoutubeService {
     }
 
     public List<YoutubeSearchVideoResponseDto> searchVideo(String searchString) {
+        return cache.get(searchString);
+    }
+
+    private List<YoutubeSearchVideoResponseDto> searchVideoInternal(String searchString) {
         BaseYoutubeResponseDto baseYoutubeResponse = youtubeSearchVideoRequestExecutor.execute(searchString);
         return baseYoutubeResponse.getItems().stream()
                 .map(youtubeInformationVideo ->
